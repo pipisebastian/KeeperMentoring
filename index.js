8ce94b0e48e56fbac6a350945af3d370ef9f10e8
pipisebastian <yrt7998@pusan.ac.kr>
2022-11-01 15:53:56 +0900

    단방향 알고리즘 문제

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const crypto = require('crypto');
 //TODO 인코딩_알고리즘 : 'base64'
 //TODO 출력_바이트 : 16
 //TODO 해시_알고리즘 : 'sha512'
+//TODO 반복횟수 : 마음대로!!!
 
 const createHashedPassword = (password) => {
     const salt = crypto.randomBytes(출력_바이트).toString(인코딩_알고리즘)
@@ -13,3 +14,5 @@ const createHashedPassword = (password) => {
 console.log(createHashedPassword("keeper"));
 console.log(createHashedPassword("keeper"));
 console.log(createHashedPassword("keeper1"));
+//TODO 3개는 모두 달라야함.
+//TODO IF, salt가 랜덤이 아니라, 고정되어있다면? keeper, keeper 결과값은 어떻게 되는가!
